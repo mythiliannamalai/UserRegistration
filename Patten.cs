2aa54a8c3d0d9db61dd77afffdcd07b4083949e2
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-31 19:58:05 +0530

    rule2

---
+++ Patten.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace patten
+{
+    class Patten
+    {
+        public Regex FirstNames = new Regex(@"^[A-z][A-Za-z]{3,}$");
+         public void Firstnamein(string Firstname)
+        {
+            Console.WriteLine("First name :"+Firstname);
+            if (FirstNames.IsMatch(Firstname))
+            {
+                Console.WriteLine(" Valid name..");
+            }else
+            {
+                Console.WriteLine("invalid name...");
+            }
+        }        
+    }
+}
