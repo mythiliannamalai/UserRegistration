2855121b752a70e9baa1ced36c12eac949eeb54c
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-31 13:52:59 +0530

    First name

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace patten
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Patten patten = new Patten();
+            Console.WriteLine("Enter the First name :");
+            string Firstname=Console.ReadLine();
+            patten.Firstnamein(Firstname);           
+        }
+    }
+}
