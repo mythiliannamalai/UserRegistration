7f968884748d2f654276a74a152e65e2b2f54bef
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-04 16:40:57 +0530

    All Patten

--- Program.cs
+++ Program.cs
@@ -5,23 +5,23 @@
         static void Main(string[]args)
         {
             UserRegistration userRegistration = new UserRegistration();
-            //Console.WriteLine("Enter the First name :");
-            //string Firstname=Console.ReadLine();
-            //Console.WriteLine("Enter the Last name :");
-            //string Lastname=Console.ReadLine();
-            //Console.WriteLine("Enter the Email id :");
-            //string Email=Console.ReadLine();
-            //Console.WriteLine("Enter the Phone number :");
-            //string PhoneNumber = Console.ReadLine();
-            //Console.WriteLine("Enter the Password :");
-            //string PassWord=Console.ReadLine();
+            Console.WriteLine("Enter the First name :");
+            string Firstname = Console.ReadLine();
+            Console.WriteLine("Enter the Last name :");
+            string Lastname = Console.ReadLine();
+            Console.WriteLine("Enter the Email id :");
+            string Email = Console.ReadLine();
+            Console.WriteLine("Enter the Phone number :");
+            string PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter the Password :");
+            string PassWord = Console.ReadLine();
             Console.WriteLine("Enter the any email patten :");
             string AllEmail=Console.ReadLine();
-            //userRegistration.Firstnamein(Firstname);
-            //userRegistration.Lastenamein(Lastname);
-            //userRegistration.Emails(Email);
-            //userRegistration.PhoneNumbers(PhoneNumber);
-            //userRegistration.PassWords(PassWord);
+            userRegistration.Firstnamein(Firstname);
+            userRegistration.Lastenamein(Lastname);
+            userRegistration.Emails(Email);
+            userRegistration.PhoneNumbers(PhoneNumber);
+            userRegistration.PassWords(PassWord);
             userRegistration.AllEmails(AllEmail);
         }
     }
